c9f6d2e066fca82d27445ad3fcfeb17146da8e6d
Byeongjoo Park <parkspider@gmail.com>
2011-10-03 02:03:47 -0700

    first project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bjbjbj::Application.config.secret_token = 'a10476f53553030a726530a65a3922d412d86c262f491d9a6109100773b86c9ee58821e8efb123b91af823443e98f4082a2d882dd2a22d6ade0f26f31b55f297'
